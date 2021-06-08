08f44a7c6e4e5ac3bd2dc489c49a51fe43bd7110
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-06-08 19:11:09 +0500

    done media chellange

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function useMedia(query) {
+  let [matches, setMatches] = useState(window.matchMedia(query).matches);
+
+  // cDM, cDU
+  useEffect(() => {
+    let media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    let listener = () => setMatches(media.matches);
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [query]);
+
+  return matches;
+}
 
 function App() {
+  let small = useMedia("(max-width: 550px)");
+  let large = useMedia("(min-width: 800px)");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Media</h1>
+      <p>Small? {small ? "Yep" : "Nope"}.</p>
+      <p>Large? {large ? "Yep" : "Nope"}.</p>
     </div>
   );
 }
